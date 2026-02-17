cda4174a7df624d0291a7290b8d494b084494d45
marquesmaycon <mayconmarquesh@gmail.com>
2026-02-17 16:41:19 -0300

    refactor: enhance layout structure and spacing in SuccessPage component

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -37,35 +37,37 @@ export default function SuccessPage() {
 
   return (
     <section className="min-h-[80vh] bg-emerald-100 pt-40 dark:bg-emerald-900">
-      <div className="container mx-auto">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-emerald-800 dark:text-emerald-400">Success!</h1>
-          <p>You just made a purchase. Check your email for the receipt and delivery details.</p>
+      <div className="container mx-auto space-y-12">
+        <div className="space-y-8">
+          <div className="space-y-4 text-center">
+            <h1 className="text-3xl font-bold text-emerald-800 dark:text-emerald-400">Success!</h1>
+            <p>You just made a purchase. Check your email for the receipt and delivery details.</p>
+          </div>
+
+          <ul className="grid gap-4 py-10 md:grid-cols-2 lg:grid-cols-3">
+            {items.map((item) => (
+              <Item key={item.id} className="max-w-full bg-indigo-200 dark:bg-emerald-950">
+                <ItemMedia>
+                  <Image
+                    src={item.image}
+                    width={200}
+                    height={200}
+                    alt="product img"
+                    className="size-20 rounded-md object-cover"
+                  />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle className="font-sans">
+                    {item.title} ({item.quantity}x) - {formatCurrency(item.price * item.quantity)}
+                  </ItemTitle>
+                  <ItemDescription>{item.description}</ItemDescription>
+                </ItemContent>
+              </Item>
+            ))}
+          </ul>
         </div>
 
-        <ul className="grid gap-4 py-10 md:grid-cols-2 lg:grid-cols-3">
-          {items.map((item) => (
-            <Item key={item.id} className="max-w-full bg-indigo-200 dark:bg-emerald-950">
-              <ItemMedia>
-                <Image
-                  src={item.image}
-                  width={200}
-                  height={200}
-                  alt="product img"
-                  className="size-20 rounded-md object-cover"
-                />
-              </ItemMedia>
-              <ItemContent>
-                <ItemTitle className="font-sans">
-                  {item.title} ({item.quantity}x) - {formatCurrency(item.price * item.quantity)}
-                </ItemTitle>
-                <ItemDescription>{item.description}</ItemDescription>
-              </ItemContent>
-            </Item>
-          ))}
-        </ul>
-
-        <div className="py-8">
+        <div className="space-y-8">
           <h2 className="text-center text-3xl font-bold text-emerald-800 dark:text-emerald-400">
             UTMs
           </h2>
